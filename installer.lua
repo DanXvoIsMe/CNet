b01f84f71b819b72280dfb6714ac76b1435d1efb
DanXvoIsMe <157852596+DanXvoIsMe@users.noreply.github.com>
2024-12-22 23:08:36 +0100

    Create installer.lua

---
+++ installer.lua
@@ -0,0 +1,4 @@
+local shell = require("shell")
+shell.execute("wget -f https://raw.githubusercontent.com/DanXvoIsMe/CNet/main/cnet/json.lua /lib/json.lua")
+shell.execute("wget -f https://raw.githubusercontent.com/DanXvoIsMe/CNet/main/cnet/cnet.lua /lib/cnet.lua")
+print("CNet library has been installed!")
