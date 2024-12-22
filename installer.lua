15706b766ca9ed70516f9331eb377e81fec67e2e
DanXvoIsMe <157852596+DanXvoIsMe@users.noreply.github.com>
2024-12-22 23:25:29 +0100

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,4 +1,5 @@
 local shell = require("shell")
 shell.execute("wget -f https://raw.githubusercontent.com/DanXvoIsMe/CNet/main/cnet/json.lua /lib/json.lua")
 shell.execute("wget -f https://raw.githubusercontent.com/DanXvoIsMe/CNet/main/cnet/cnet.lua /lib/cnet.lua")
+shell.execute("wget -f https://raw.githubusercontent.com/DanXvoIsMe/CNet/main/cnet/asciiencryption.lua /lib/asciiencryption.lua")
 print("CNet library has been installed!")
